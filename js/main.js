cf053180534e19ce1541717bfaba2d284585feec
rocco-de-luca <roccodelucaboolean@gmail.com>
2020-03-25 16:59:47 +0100

    js-pwdgen-wannabe

---
+++ js/main.js
@@ -0,0 +1,19 @@
+//I ask the user for his name and then memorize it
+
+var name = prompt('What\'s your name?');
+console.log(name);
+document.getElementById('name').innerHTML = name
+
+//I ask the user for his surname and then memorize it
+
+var surname = prompt('What\'s your surname?');
+console.log(surname);
+document.getElementById('surname').innerHTML = surname
+
+//I ask the user for his favorite color and then memorize it
+
+var color = prompt('What\'s your favorite color?');
+console.log(color);
+
+document.getElementById('color').innerHTML = color
+
